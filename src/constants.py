755110e07c39150065a1c0ebb788fe0ab64195e8
Pitehr Hurtado-Cayo <phurtado@demafront.cl>
2025-11-14 12:53:25 -0300

    update: etl pos precommits

---
+++ src/constants.py
@@ -0,0 +1,26 @@
+"""Constants for the application."""
+
+# Constants
+FEE_COST_FROM_SATELLITE = 0.00001333
+FEE_COST_FROM_DC = 0.264
+
+
+# PATH_ROOT = "./data/"
+PATH_ROOT = "/Users/pitehrhurtado/Documents/Operation-Researchs/network-design-2e-last-mile/data/"
+
+# Paths
+PATH_DATA_SATELLITE = PATH_ROOT + "input_satellites.xlsx"
+PATH_DATA_PIXEL = PATH_ROOT + "input_pixels.xlsx"
+PATH_DATA_DISTANCES_FROM_SATELLITES = PATH_ROOT + "input_matrix_distance_satellite_to_pixels.xlsx"
+PATH_DATA_DISTANCES_FROM_DC = PATH_ROOT + "input_matrix_distance_dc_to_pixels.xlsx"
+
+# Path root from scenarios
+PATH_ROOT_SCENARIO = PATH_ROOT + "scenarios/"
+
+PATH_SAMPLING_SCENARIO = PATH_ROOT_SCENARIO + "sampling/"
+PATH_BEST_SOLUTION_SAA = PATH_ROOT_SCENARIO + "best_solution_saa/"
+
+# distances matrix
+PATH_DATA_DISTANCES_FACILITY_DELIVERY_ZONE = PATH_ROOT + "distances/input_matrix_distance_satellite_to_pixels.xlsx"
+
+PATH_DATA_DISTANCES_FACILITIES = PATH_ROOT + "distances/input_matrix_distance_dc_to_pixels.xlsx"
--- src/data/etl.py
+++ src/data/etl.py
@@ -1,16 +1,15 @@
-'''Module for ETL processes related to distance data.'''
+"""Module for ETL processes related to distance data."""
 
 import pandas as pd
-from src.constants import (
-    PATH_DATA_DISTANCES_FACILITY_DELIVERY_ZONE,
-    PATH_DATA_DISTANCES_FACILITIES
-)
+
+from src.constants import PATH_DATA_DISTANCES_FACILITIES, PATH_DATA_DISTANCES_FACILITY_DELIVERY_ZONE
 from src.utils.custom_logger import get_logger
+
 logger = get_logger(__name__)
 
 
 def get_distance_facility_delivery_zone() -> dict:
-    '''Get distances between facilities and delivery zones from an external file.'''
+    """Get distances between facilities and delivery zones from an external file."""
     try:
         distance_facility_delivery_zone = {}
 
@@ -18,7 +17,7 @@ def get_distance_facility_delivery_zone() -> dict:
         for _, row in df.iterrows():
             i = row["id_satellite"]
             j = row["id_pixel"]
-            distance = row["distance"]/1000
+            distance = row["distance"] / 1000
             distance_facility_delivery_zone[(i, j)] = distance
 
         return distance_facility_delivery_zone
@@ -26,15 +25,18 @@ def get_distance_facility_delivery_zone() -> dict:
         logger.error(f"File {PATH_DATA_DISTANCES_FACILITY_DELIVERY_ZONE} not found")
         raise error
 
+
 def get_distance_facilities() -> dict:
-    '''Get distances between facilities from an external file.'''
+    """Get distances between facilities from an external file."""
     try:
         distance_facilities = {}
 
         df = pd.read_excel(PATH_DATA_DISTANCES_FACILITIES)
         for _, row in df.iterrows():
-            i = row["id_pixel"] # TODO check because this should be id_satellite, We need to estimate distance from DC to satellite
-            distance = row["distance"]/1000
+            i = row[
+                "id_pixel"
+            ]  # TODO check because this should be id_satellite, We need to estimate distance from DC to satellite
+            distance = row["distance"] / 1000
             distance_facilities[i] = distance
 
         return distance_facilities
